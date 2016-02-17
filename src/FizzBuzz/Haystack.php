ad44ccdf97cc57f435678aa3fc04c213de714634
John Kary <john@johnkary.net>
2016-02-17 02:31:42 -0600

    Namespace by kata allowing auto-registering in test suite
    Developers can create any implementation of a kata by creating a class
    in the kata's directory and implementing the kata's interface.

---
+++ src/FizzBuzz/Haystack.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RethinkingLoops\FizzBuzz;
+
+use RethinkingLoops\FizzBuzz;
+
+/**
+ * FizzBuzz implementation using the Haystack library.
+ */
+class Haystack implements FizzBuzz
+{
+    /**
+     * @return mixed[] Array of numbers, Fizz, Buzz and FizzBuzz
+     */
+    public function run()
+    {
+        // TODO: Implement run() method.
+    }
+}
